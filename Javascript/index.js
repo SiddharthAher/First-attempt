3ee8b313a6d228a91ebc7d5173610c8ad8b39dba
SiddharthAher <siddharthaaher@gmail.com>
2021-08-11 18:18:03 +0530

    css design

---
+++ Javascript/index.js
@@ -0,0 +1,29 @@
+//let selectedColor = ['red','green','blue'];
+
+//console.log(selectedColor.length);
+
+/*function greet(name){
+    console.log('hello '+ name );
+
+}
+
+ greet('ravi');
+ */
+
+ /*function greet(name,lastName){
+     console.log('hello ' + name + ' ' + lastName);
+ }
+
+ greet('john','smith');
+ */
+
+ /*function square(number)
+{
+    return number*number;
+}
+console.log(square(8));
+*/
+function greet(name,lastName){
+    console.log('hello '+ name + ''+ lastName);
+    
+}
